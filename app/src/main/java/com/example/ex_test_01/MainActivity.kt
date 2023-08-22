1807deea0e2b56bb129269bc744a22daaa449580
4seokhyeon <tktjrgus@naver.com>
2023-08-22 14:38:34 +0900

    데이터 클래스

--- app/src/main/java/com/example/ex_test_01/MainActivity.kt
+++ app/src/main/java/com/example/ex_test_01/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.ex_test_01
 import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ex_test_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,23 +21,24 @@ class MainActivity : AppCompatActivity() {
 
         // 데이터 원본 준비
         val dataList = mutableListOf<MyItem>()
-        dataList.add(MyItem(R.drawable.dark_header, "Bella", "1"))
-        dataList.add(MyItem(R.drawable.ic_delete, "Charlie", "2"))
-        dataList.add(MyItem(R.drawable.dark_header, "Daisy", "1.5"))
-        dataList.add(MyItem(R.drawable.dark_header, "Duke", "1"))
-        dataList.add(MyItem(R.drawable.alert_light_frame, "Max", "2"))
-        dataList.add(MyItem(R.drawable.dialog_holo_dark_frame, "Happy", "4"))
-        dataList.add(MyItem(R.drawable.arrow_down_float , "Luna", "3"))
-        dataList.add(MyItem(R.drawable.btn_default_small, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.arrow_down_float, "Bella", "1"))
+        dataList.add(MyItem(R.drawable.alert_light_frame, "Charlie", "2"))
+        dataList.add(MyItem(R.drawable.dialog_frame, "Daisy", "1.5"))
+        dataList.add(MyItem(R.drawable.divider_horizontal_bright, "Duke", "1"))
+        dataList.add(MyItem(R.drawable.arrow_down_float, "Max", "2"))
+        dataList.add(MyItem(R.drawable.dialog_frame, "Happy", "4"))
+        dataList.add(MyItem(R.drawable.ic_media_ff, "Luna", "3"))
+        dataList.add(MyItem(R.drawable.dialog_holo_dark_frame, "Bob", "2"))
 
+        val adapter = MyAdapter(dataList)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // 어댑터 생성 및 연결
-        binding.listView.adapter = MyAdapter(this, dataList)
-
-        // 항목 클릭 이벤트 처리
-        binding.listView.setOnItemClickListener{ parent, view, position, id ->
-            val name: String = (binding.listView.adapter.getItem(position) as MyItem).aName
-            Toast.makeText(this," $name 선택!", Toast.LENGTH_SHORT).show()
+        adapter.itemClick = object : MyAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val name: String = dataList[position].aName
+                Toast.makeText(this@MainActivity," $name 선택!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
